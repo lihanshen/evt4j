25e45017f2251dab130885650089a602e382c755
Fei Liu <liu.feiwood@gmail.com>
2019-01-28 22:34:37 +0100

    add api call of push transaction

---
+++ src/main/java/io/everitoken/sdk/java/dto/TransactionData.java
@@ -0,0 +1,35 @@
+package io.everitoken.sdk.java.dto;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
+
+public class TransactionData implements Transactable {
+    private final String trxId;
+    private final JSONObject processed;
+
+    private TransactionData(String trxId, JSONObject processed) {
+        this.trxId = trxId;
+        this.processed = processed;
+    }
+
+    @NotNull
+    @Contract("_ -> new")
+    public static TransactionData ofRaw(@NotNull JSONObject raw) {
+        return new TransactionData(
+                raw.getString("transaction_id"),
+                raw.getJSONObject("processed")
+        );
+    }
+
+    @Override
+    @JSONField(name = "transaction_id")
+    public String getTrxId() {
+        return trxId;
+    }
+
+    public JSONObject getProcessed() {
+        return processed;
+    }
+}
