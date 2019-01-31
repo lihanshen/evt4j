1e81da170fc3c8d4d6787450ed0b689c606e5568
Fei Liu <liu.feiwood@gmail.com>
2019-01-31 10:17:17 +0100

    add requiredKeys api

---
+++ src/main/java/io/everitoken/sdk/java/apiResources/SigningRequiredKeys.java
@@ -0,0 +1,27 @@
+package io.everitoken.sdk.java.apiResources;
+
+import com.mashape.unirest.http.JsonNode;
+import io.everitoken.sdk.java.exceptions.ApiResponseException;
+import io.everitoken.sdk.java.params.RequestParams;
+import org.json.JSONArray;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class SigningRequiredKeys extends ApiResource {
+    private static final String uri = "/v1/chain/get_required_keys";
+
+    SigningRequiredKeys() {
+        super(uri);
+    }
+
+    public List<String> request(RequestParams requestParams) throws ApiResponseException {
+        JsonNode res = super.makeRequest(requestParams);
+        JSONArray array = res.getObject().getJSONArray("required_keys");
+
+        return StreamSupport.stream(
+                array.spliterator(), true).map(key -> (String) key).collect(Collectors.toList()
+        );
+    }
+}
