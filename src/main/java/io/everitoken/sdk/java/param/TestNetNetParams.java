6df919e6b5e89f4f0ebf6395798a2672cb3667a4
Fei Liu <liu.feiwood@gmail.com>
2019-03-21 22:32:12 +0100

    update testnet port

--- src/main/java/io/everitoken/sdk/java/param/TestNetNetParams.java
+++ src/main/java/io/everitoken/sdk/java/param/TestNetNetParams.java
@@ -2,6 +2,6 @@ package io.everitoken.sdk.java.param;
 
 public class TestNetNetParams extends NetParams {
     public TestNetNetParams() {
-        super("https", NET.TESTNET.getUrl(), 8888, 15000);
+        super("https", NET.TESTNET.getUrl(), 443, 15000);
     }
 }
